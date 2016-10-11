70cf7300f62214c8b2e56c98bf2852a92e716f8c
Dylan Avery <fossilfuelmusic@gmail.com>
2016-10-11 16:08:24 -0600

    end of day commit

--- js/numberguesser.js
+++ js/numberguesser.js
@@ -4,20 +4,24 @@ function randomNum() {
    }
 
 var randomNumber = randomNum();
+var min = 1;
+var max = 100;
 
-
+var h2 = document.querySelector('h2')
+var guess = document.querySelector('.guess');
+var clear = document.querySelector('.clear');
+var userInput = document.querySelector('#userInput');
+var reset = document.querySelector('.reset');
+var lastGuess = document.querySelector('.lastGuess');
+var too = document.querySelector('#too');
 
 function mainFunction() {
- var randomNumber = randomNum();
+  randomNumber = randomNum();
   return(randomNumber);
 }
 
-// function inputCheck() {
-//    compareGuess();
-// }
-
 function displayGuess() {
-  h2.innerText("Your last guess was");
+  h2.innerText = "Your last guess was";
 }
 
 function compareGuess() {
@@ -26,28 +30,43 @@ function compareGuess() {
   if (userGuess === randomNum) {
      too.innerText = 'Well now look at you!';
 }
-  if (userGuess < randomNum) {
+
+else if (userGuess > max) {
+  too.innerText = 'Please enter a number between 1-100';
+}
+else if (userGuess < randomNum) {
     too.innerText = 'That is too low.';
 }
   if (userGuess > randomNum) {
     too.innerText = 'That is too high.';
 }
+  else if (isNaN(userGuess)) {
+    too.innerText = 'Do not put letters in your number';
+  }
+
+  // else if (userGuess < 1 || userGuess > 100) {
+  //   too.innerText = 'Please enter a number between 1-100';
+  // }
 
 }
+
+function checkRange() {
+  var userGuess = getGuess();
+  var randomNum = randomNumber;
+  if (userGuess < 1 || userGuess > 100) {
+    too.innerText = 'Please enter a number between 1-100';
+  }
+}
+
+
+
 
 function getGuess() {
   var userGuess=Number(document.getElementById('userInput').value);
   return(userGuess);
 }
 
-var h2 = document.querySelector('h2')
 
-var guess = document.querySelector('.guess');
-var clear = document.querySelector('.clear');
-var userInput = document.querySelector('#userInput');
-var reset = document.querySelector('.reset');
-var lastGuess = document.querySelector('.lastGuess');
-var too = document.querySelector('#too');
 
 
 
@@ -58,14 +77,17 @@ guess.addEventListener('click', function () {
       var userGuess = getGuess();
       // displayGuess();
       lastGuess.innerText = userGuess;
+      displayGuess();
       compareGuess();
+      checkRange();
    // put this into a display function? ^
 });
 clear.addEventListener('click', function () {
-      getGuess();
-      lastguess.innerText = "";
+      lastGuess.innerText = "";
+      too.innerText = "";
+      h2.innerText = "";
+      userInput.value = "";
 });
 reset.addEventListener('click', function() {
      mainFunction();
-
 });
